251f8b21ab56f46d7cc4e26ae92c5a408dffacf8
AurelieDombre <au.dombre@gmail.com>
2022-10-24 16:44:02 +0200

    ajout d'un projet

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -7,13 +7,13 @@ const bubbleDisplay = {
         // la méthode init permet de poser tous les espion: l'initialisation du module
         
         // on place un ecouteur sur l'event 'scroll' de la fenetre 'window'
-        //  event scroll : https://developer.mozilla.org/fr/docs/Web/API/Document/scroll_event
+        //  event scroll : https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
         window.addEventListener("scroll", bubbleDisplay.handleScroll);
 
         // on veut lancer un timer au chargment du site pour afficher la bulle au bout de 5 secondes !
         // premier argument : le handler
         // deuxième argument : le nombre de millisecondes à attendre avant d'executer le handler
-        setTimeout(bubbleDisplay.display, 8000);
+        setTimeout(bubbleDisplay.display, 5000);
     },
 
 
@@ -21,13 +21,13 @@ const bubbleDisplay = {
      * Automatisation de l'affichage de la bulle de contact
      */
     handleScroll: function () {
-        // si window.scrollY dépasse 100 on affiche la bulle
+        // si window.scrollY dépasse 150px on affiche la bulle
         if (window.scrollY > 150) {
             // on affiche la bulle !!!
             bubbleDisplay.display();
 
             // on renvoie l'espion chez lui, on veut supprimer le eventListener !!!!
-            // doc : https://developer.mozilla.org/fr/docs/Web/API/EventTarget/removeEventListener
+            // doc : https://developer.mozilla.org/en/docs/Web/API/EventTarget/removeEventListener
             window.removeEventListener("scroll", bubbleDisplay.handleScroll);
         }
     },
